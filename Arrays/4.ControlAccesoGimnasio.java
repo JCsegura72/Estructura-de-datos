284adc2d08cf1f5e5020656c1197dfda17c5f7be
JCsegura72 <seguracamilo721@gmail.com>
2026-03-03 15:41:13 -0500

    Agrego ejercicios de Estructura de Datos

---
+++ Arrays/4.ControlAccesoGimnasio.java
@@ -0,0 +1,39 @@
+public class ControlAccesoGimnasio {
+
+//4. Control de Acceso (Gimnasio)
+//Un gimnasio tiene una lista de códigos de acceso de los socios que pagaron la mensualidad. La lista está ordenada de menor a mayor.
+//El Problema: Cuando un socio digita su código, el sistema debe validar si el código está en la lista de "pagos al día". Si no está, se le niega la entrada.
+//Algoritmo a usar: Búsqueda Binaria. Es eficiente para buscar códigos numéricos en una lista que ya está organizada.
+   
+    public static void main(String[] args) {
+
+        int[] sociosActivos = {1001, 1020, 1055, 1100, 1200, 1305, 1500};
+        int codigoIngresado = 1100;
+
+        int izquierda = 0;
+        int derecha = sociosActivos.length - 1;
+        boolean autorizado = false;
+
+        while (izquierda <= derecha) {
+
+            int medio = (izquierda + derecha) / 2;
+
+            if (sociosActivos[medio] == codigoIngresado) {
+                autorizado = true;
+                break;
+            } 
+            else if (sociosActivos[medio] < codigoIngresado) {
+                izquierda = medio + 1;
+            } 
+            else {
+                derecha = medio - 1;
+            }
+        }
+
+        if (autorizado) {
+            System.out.println("Acceso permitido. Bienvenido al gimnasio 💪");
+        } else {
+            System.out.println("Acceso denegado. Mensualidad pendiente.");
+        }
+    }
+}
